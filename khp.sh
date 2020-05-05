34fd5bcc79d5c6197f1aae7ce4c9a8da58d1536d
khp11 <64519272+khp11@users.noreply.github.com>
2020-05-05 17:37:57 +0530

    Add files via upload

--- khp.sh
+++ khp.sh
@@ -24,7 +24,7 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet khp
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpeedX
 echo -e "\e[4;34m This Bomber Was Created By khp official anonymous \e[0m"
@@ -44,14 +44,14 @@ if [ $ch -eq 1 ];then
 clear
 echo -e "\e[1;32m"
 rm *.xxx >/dev/null 2>&1
-python3 bomber.py
+python3 khp.py
 rm *.xxx >/dev/null 2>&1
 exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
 echo 'Call  By khp'> call.xxx
-python3 bomber.py call
+python3 khp.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
 elif [ $ch -eq 3 ];then
@@ -59,7 +59,7 @@ clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
 git clone https://github.com/khp11/khp
-if [[ -s khp/TBomb.sh ]];then
+if [[ -s khp/khp.sh ]];then
 cd khp
 cp -r -f * .. > temp
 cd ..
@@ -77,7 +77,7 @@ exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBomb
+figlet khp
 echo -e "\e[1;34mCreated By \e[1;34m"
 toilet -f mono12 -F border anonymous
 echo  " "
@@ -115,7 +115,7 @@ clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet khp
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpkhpX
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
